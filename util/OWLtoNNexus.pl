be0a139b0903a7463621f90512a5eb176ee8c59d
Deyan Ginev <d.ginev@jacobs-university.de>
2012-07-14 00:38:19 +0200

    refactoring owl importer

--- util/OWLtoNNexus.pl
+++ util/OWLtoNNexus.pl
@@ -1,25 +1,19 @@
 #!/usr/bin/perl
 use strict;
-
-use Boost::Graph;
-use Data::Dumper;
+use warnings;
+use Graph;
 use RDF::Simple::Parser;
 use DBI;
 
 my $infile = $ARGV[0];
-
 my $databasename = $ARGV[1];
 
 open ( IN, $infile );
 
 my @data = <IN>;
-
 my $text = join ('', @data);
-
 my $parser = RDF::Simple::Parser->new();
-
 my @triples = $parser->parse_rdf($text);
-
 
 my $upg = new Boost::Graph( directed => 0 );
 foreach my $t ( @triples ) {
@@ -33,79 +27,36 @@ foreach my $t ( @triples ) {
 	}
 }
 
-my $i = 0;
-my $p = 'root';
-my $list = $upg->neighbors($p);
-my %prev = ();
-while ( 1 ) {
-	$i++;
-	my @children;
-	foreach my $t ( @$list ) {
-		my $c = $upg->neighbors($t);
-		foreach my $j ( @$c ) {
-			if ( not  defined ( $prev{$j} ) ) {
-				push @children, $j;
-				$prev{$j} = 1;
-			}
-		}
-	}
-	@children = uniquify( @children );
-	if ( @children < 1 ) {
-		last;	
-	}
-	#%prev = map { $_ => 1 } @$list;
-	$list = \@children;
+### Rewrite traversal and SQL insertion:
+
+my $vertex_queue = [ ['root',0] ];
+my $edge_collection = [];
+my $visited = {};
+my $max_depth = 0;
+
+while ( @$vertex_queue ) {
+  # 0. Grab a current vertex from the queue
+  my $current_vertex = shift @$vertex_queue;
+  $max_depth = $current_vertex->[1] if ($current_vertex->[1] > $max_depth);
+  # 1. Visit
+  next if $visited->{$current_vertex->[0]};
+  $visited->{$current_vertex->[0]} = 1;
+  # 2. Push new neighbors to queue, increment level
+  my $neighbors = $upg->neighbors($current_vertex->[0]);
+  foreach my $neighbor ( @$neighbors ) {
+    if ( ! defined ( $visited->{$neighbor->[0]} ) ) {
+      # 3. Add an edge for every neighbour if not visited,
+      # to avoid adding twice (undirected graph)
+      my $next_level = $current_vertex->[1]+1;
+      push @$edge_collection, [$neighbor, $current_vertex->[0] , 10**($next_level) ];
+      # 4. Push the neighbour on the queue
+      push_if_new($vertex_queue, [$neighbor,$next_level]);
+    }
+  }
 }
 
-my $levels = $i;
-
-print "There are $levels levels\n";
-
-
-my $topweight = 10**($i-1);
-
-#now assign the appropriate weights to the graphs.
-#my $list = $upg->children_of_directed($p);
-my $i = 0;
-my $p = 'root';
-my $list = $upg->neighbors($p);
-my @edges = ();
-my %prev = ();
-my @children = ();
-$prev{'root'} = 1;
-foreach my $t ( @$list ) {
-	my $weight = $topweight;
-	my @edge = ( "$t", "$p", "$weight" );
-	push @edges, \@edge;
-	print "$t $p $weight\n";
-	$prev{$t} = 1;
-	push @children, $t;
-}
-
-
-$i=1;
-while ( 1 ) {
-	my @children;
-	foreach my $t ( @$list ) {
-		my $c = $upg->neighbors($t);
-		foreach my $j ( @$c ) {
-			if ( not  defined ( $prev{$j} ) ) {
-				my $weight = $topweight / (10**$i);
-				my @edge = ( "$j", "$t", "$weight" );
-				push @edges, \@edge;
-				print "$j $t $weight\n";
-				push @children, $j;
-				$prev{$j} = 1;
-			}
-		}
-	}
-	@children = uniquify( @children );
-	if ( @children < 1 ) {
-		last;	
-	}
-	$list = \@children;
-	$i++;
-}
+print "There are $max_depth levels\n";
+my $topweight = 10**($max_depth-1);
 
 my $dbh = DBI->connect( "DBI:mysql:$databasename;localhost", "nnexus", "nnexus", 
 			{RaiseError=>1}) || die "coundn't connect" ;
@@ -115,15 +66,15 @@ my $del = $dbh->prepare( "delete from ontology" );
 $del->execute();
 my $sth = $dbh->prepare( "insert into ontology ( child, parent, weight ) values ( ?, ?, ?)" );
 
-foreach my $e ( @edges ) {
+foreach my $e ( @$edge_collection ) {
 	my ($c, $p, $w) = @$e;
-	$sth->execute( $c, $p, $w);
+	$sth->execute( $c, $p, $topweight/$w);
 }
 
-
-sub uniquify {
-        my @a = @_;
-	my %seen = ();
-	my @uniqu = grep { ! $seen{$_} ++ } @a;
-        return @uniqu;
+# Pushes element in array if new
+# Note: All elements are of the form [$name,$number]
+#       where the $name-s are the important comparables
+sub push_if_new {
+  my ($array,$element) = @_;
+  push @$array, $element unless grep {$element->[0] eq $_->[0]} @$array;
 }
