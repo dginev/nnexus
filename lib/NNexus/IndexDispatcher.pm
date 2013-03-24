fa047f92f92b2d37c4e4c0d1afedeac88c868b68
Deyan Ginev <d.ginev@jacobs-university.de>
2013-03-24 16:23:11 -0300

    pod for IndexDispatcher (soon Index::Dispatcher)

--- lib/NNexus/IndexDispatcher.pm
+++ lib/NNexus/IndexDispatcher.pm
@@ -2,7 +2,7 @@ package NNexus::IndexDispatcher;
 use warnings;
 use strict;
 
-# 1. Set all default values, 
+# Dispatch to the right NNexus::Index::Domain class
 sub new {
 	my ($class,$domain) = @_;
 	$domain = $domain ? ucfirst(lc($domain)) : 'Planetmath';
@@ -16,9 +16,49 @@ sub new {
 		require NNexus::Index::Template;
 		# The generic template will always fail...
 		# TODO: Should we fallback to Planetmath instead?
-		NNexus::Index::Template->new();
+		NNexus::Index::Template->new;
 	}
 }
 
 1;
 __END__
+
+=pod
+
+=head1 NAME
+
+C<NNexus::Index::Dispatcher> - High-level dispatcher to the correct domain indexer classes.
+
+=head1 SYNOPSIS
+
+use NNexus::Index::Dispatcher;
+my $indexer = NNexus::Index::Dispatcher->new($domain);
+
+=head1 DESCRIPTION
+
+Simple interface to obtaining the correct indexer class for a given domain.
+
+TODO: The intention is to also extend the capabilities of the dispatcher to auto-detect and report on
+  the available domain indexers and their properties (Meta-object Programming (MOP)).
+
+=head2 METHODS
+
+=over 4
+
+=item C<< my $indexer = NNexus::Index::Dispatcher->new($domain); >>
+
+Returns an object of NNexus::Index::ucfirst(lc($domain)) if available, or the generic Template object otherwise.
+
+=back
+
+=head1 AUTHOR
+
+Deyan Ginev <d.ginev@jacobs-university.de>
+
+=head1 COPYRIGHT
+
+Research software, produced as part of work done by
+the KWARC group at Jacobs University Bremen.
+Released under the GNU Public License
+
+=cut
