92a6901117597976ba6e1b696dd700ec4280ed48
Deyan Ginev <d.ginev@jacobs-university.de>
2013-03-24 22:34:31 -0400

    removing old deprecated utility files, if anything is needed they are kept in _Old_NNexus

--- util/OAIHarvester/NoosphereHarvester.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/perl
-
-my $baseURL = "http://mathdl.maa.org/partnerOAI";
-
-`perl OAIHarvester.pl $baseURL oai_dc "MathWorld" > mathworld.xml`;
-`perl OAIHarvester.pl $baseURL oai_dc "MathResources Inc." > mathresources.xml`;
-
