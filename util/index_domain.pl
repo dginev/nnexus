2544f2645b811eea9593ef1c836b1b098c76baa9
Deyan Ginev <d.ginev@jacobs-university.de>
2013-04-29 00:13:28 -0400

    renaming the utility to index_domain, specialized in single domain indexing

--- util/index_domain.pl
+++ util/index_domain.pl
@@ -14,8 +14,10 @@ use Data::Dumper;
 use Mojo::DOM;
 my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
 my $date = $mday.'-'.($mon+1).'-'.(1900+$year);
+
+my $domain = ucfirst(lc(shift));
 #my $dbname = "wiki-$date.db";
-my $dbname = "Wikipedia.db";
+my $dbname = "$domain.db";
 #unlink $dbname if (-e $dbname);
 my $options = {
   "database" => {
@@ -30,13 +32,11 @@ my $options = {
 my $db = NNexus::DB->new(%{$options->{database}});
 
 # 2. Index all sites, showing intermediate progress
-foreach my $domain(qw/Wikipedia/) {
-  my $index_job = NNexus::Job->new(function=>'index',verbosity=>1,
-				   url=>'default',domain=>$domain,db=>$db,should_update=>0);
-  $index_job->execute;
-  my $response = $index_job->response;
-  print STDERR Dumper($response);
-}
+my $index_job = NNexus::Job->new(function=>'index',verbosity=>1,
+		   url=>'default',domain=>$domain,db=>$db,should_update=>0);
+$index_job->execute;
+my $response = $index_job->response;
+print STDERR Dumper($response);
 
 # 3. Create DB Dump
 # TODO
