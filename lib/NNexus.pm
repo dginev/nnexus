52cfe568654920edbd74b2c0e9217c2bec48a056
Deyan Ginev <d.ginev@jacobs-university.de>
2014-02-18 14:02:07 +0100

    releasing 2.0 and officially moving on to the 2.1 milestone

--- lib/NNexus.pm
+++ lib/NNexus.pm
@@ -24,7 +24,7 @@ our @EXPORT = qw(linkentry indexentry);
 our ($INSTALLDIR) = grep(-d $_, map("$_/NNexus", @INC));
 
 use vars qw($VERSION);
-$VERSION  = "2.0alpha3";
+$VERSION  = "2.0";
 
 use Mojo::JSON 'j';
 use NNexus::DB;
